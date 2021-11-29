b59d00addfc5ad797a31b52a96f6dda329be84e5
Romain <lapierre.romain@gmail.com>
2021-11-29 16:31:30 -0500

    Add fonts splashscreen and some components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Splashscreen from './components/Splashscreen/Splashscreen';
+import Hero from './components/Hero/Hero';
+import HeroesList from './components/HeroesList/HeroesList';
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-container">
+          <Splashscreen display={true}></Splashscreen>
+          <Hero></Hero>
+          <HeroesList></HeroesList>
+        </div>
       </header>
     </div>
   );
