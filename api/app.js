a1a4b2ff24eae2385fa1bf1b7e3421d8a8c773bf
Romain <lapierre.romain@gmail.com>
2021-11-29 15:51:38 -0500

    Remove unused files

--- api/app.js
+++ api/app.js
@@ -20,14 +20,9 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', heroesRouter);
